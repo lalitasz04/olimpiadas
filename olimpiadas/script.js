5c6a66223f5158eb2df032fc7d758db17e6c9e97
lalitasz04 <132698698+lalitasz04@users.noreply.github.com>
2023-09-13 18:29:30 -0300

    Add files via upload

---
+++ olimpiadas/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add('navbar-scrolled');
+        } else {
+            navbar.classList.remove('navbar-scrolled');
+        }
+    });
+});
+
