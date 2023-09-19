0310dba1e5859eb3f542f106c3c3344af3f4a551
lalitasz04 <132698698+lalitasz04@users.noreply.github.com>
2023-09-19 19:08:04 -0300

    Add files via upload

---
+++ pruebitas1/guar.php
@@ -0,0 +1,29 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "olimpiadas");
+
+// Verifica si la conexión a la base de datos fue exitosa
+if ($conexion->connect_error) {
+    die("Error en la conexión a la base de datos: " . $conexion->connect_error);
+}
+
+// Obtiene los datos enviados desde el formulario
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Consulta SQL para buscar el usuario y la contraseña en la base de datos
+$query = "SELECT * FROM usuario WHERE username = '$username' AND password = '$password'";
+$resultado = $conexion->query($query);
+
+// Verifica si se encontró un usuario con la contraseña proporcionada
+if ($resultado->num_rows === 1) {
+    // Usuario y contraseña son correctos, redirige al usuario a la página deseada
+    header("Location: index.html");
+    exit;
+} else {
+    // Si las credenciales son incorrectas, muestra un mensaje de error
+    echo "Usuario o contraseña incorrectos. Inténtalo de nuevo.";
+}
+
+// Cierra la conexión a la base de datos
+$conexion->close();
+?>
